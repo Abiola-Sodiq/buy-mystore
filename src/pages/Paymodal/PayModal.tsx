8c1cf7869afe81263c8e963c4de4763c1681c06c
Abiola-Sodiq <rasababe01@gmail.com>
2024-06-13 16:58:37 +0100

    payment  modal fixes

--- src/pages/Paymodal/PayModal.tsx
+++ src/pages/Paymodal/PayModal.tsx
@@ -3,6 +3,8 @@
 import { Button, Form, Input, Modal } from "antd";
 import { useState } from "react";
 import { formConfig } from "../../utils/helper";
+import success from "../../assets/icons/success-svgrepo-com.svg";
+import failed from "../../assets/icons/error-svgrepo-com.svg";
 
 type Props = {
   amount: number;
@@ -18,10 +20,12 @@ declare global {
 const PayModal = ({ amount, onClose }: Props) => {
   const [form] = Form.useForm();
   const [openModal, setOpenModal] = useState<boolean>(true);
+  const [onPaymentSuccess, setOnPaymentSuccess] = useState<
+    "success" | "failed" | "default"
+  >("default");
 
   const handlePaystackSuccess = () => {
-    console.log("Payment complete!");
-    setOpenModal(false);
+    setOnPaymentSuccess("success");
   };
 
   const handleFormFinish = (values: any) => {
@@ -32,7 +36,7 @@ const PayModal = ({ amount, onClose }: Props) => {
       currency: "NGN",
       callback: handlePaystackSuccess,
       onClose: () => {
-        alert("Transaction was not completed, window closed.");
+        setOnPaymentSuccess("failed");
       },
     });
 
@@ -44,64 +48,86 @@ const PayModal = ({ amount, onClose }: Props) => {
       open={openModal}
       onCancel={() => {
         setOpenModal(false);
-        onClose(); 
+        onClose();
       }}
       footer={null}
       className="p-6 space-y-4"
     >
-      <Form
-        form={form}
-        {...formConfig}
-        fields={[
-          {
-            name: "email",
-            value: "",
-          },
-          {
-            name: "amount",
-            value: amount,
-          },
-        ]}
-        onFinish={handleFormFinish}
-      >
-        <Form.Item
-          label={
-            <span className="text-base font-semibold text-[#333333] mt-6 ">
-              Please Enter E-mail Address to proceed with payment
-            </span>
-          }
-          name="email"
-          rules={[
-            { required: true, message: "Email is required" },
-            { type: "email", message: "Invalid Email Address" },
+      {onPaymentSuccess === "default" && (
+        <Form
+          form={form}
+          {...formConfig}
+          fields={[
+            {
+              name: "email",
+              value: "",
+            },
+            {
+              name: "amount",
+              value: amount,
+            },
           ]}
+          onFinish={handleFormFinish}
         >
-          <Input
-            className="w-full rounded-xl border border-[#E3E6EA] !py-4 placeholder:!text-base placeholder:font-normal placeholder:text-[#666666]"
-            placeholder="Enter your email address"
-          />
-        </Form.Item>
-        <Form.Item
-          label={
-            <span className="text-base font-normal text-[#333333]">Amount</span>
-          }
-          name="amount"
-          rules={[{ required: true, message: "Amount is required" }]}
-        >
-          <Input
-            className="w-full rounded-xl border border-[#E3E6EA] !py-4 placeholder:!text-base placeholder:font-normal placeholder:text-[#666666]"
-            disabled
-            prefix="₦"
-          />
-        </Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          className=" w-full py-6 bg-stone-600 !text-white rounded-xl text-lg  flex justify-center items-center"
-        >
-          Proceed to Pay
-        </Button>
-      </Form>
+          <Form.Item
+            label={
+              <span className="text-base font-semibold text-[#333333] mt-6 ">
+                Please Enter E-mail Address to proceed with payment
+              </span>
+            }
+            name="email"
+            rules={[
+              { required: true, message: "Email is required" },
+              { type: "email", message: "Invalid Email Address" },
+            ]}
+          >
+            <Input
+              className="w-full rounded-xl border border-[#E3E6EA] !py-4 placeholder:!text-base placeholder:font-normal placeholder:text-[#666666]"
+              placeholder="Enter your email address"
+            />
+          </Form.Item>
+          <Form.Item
+            label={
+              <span className="text-base font-normal text-[#333333]">
+                Amount
+              </span>
+            }
+            name="amount"
+            rules={[{ required: true, message: "Amount is required" }]}
+          >
+            <Input
+              className="w-full rounded-xl border border-[#E3E6EA] !py-4 placeholder:!text-base placeholder:font-normal placeholder:text-[#666666]"
+              disabled
+              prefix="₦"
+            />
+          </Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className=" w-full py-6 bg-stone-600 !text-white rounded-xl text-lg  flex justify-center items-center"
+          >
+            Proceed to Pay
+          </Button>
+        </Form>
+      )}
+      {onPaymentSuccess === "success" && (
+        <div className=" flex justify-center items-center flex-col gap-6">
+          <img className=" h-20 w-20" src={success} alt="" />
+          <p className=" text-[#333333] text-xl text-center">
+            Payment complete!
+          </p>
+        </div>
+      )}
+      {onPaymentSuccess === "failed" && (
+        <div>
+          <div className=" flex justify-center items-center flex-col gap-6">
+            <img className=" h-20 w-20" src={failed} alt="" />
+            <p className=" text-[#333333] text-xl text-center">
+              Transaction was not completed, window closed.
+            </p>
+          </div>
+        </div>
+      )}
     </Modal>
   );
 };
